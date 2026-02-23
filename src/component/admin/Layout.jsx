df0e3ef75d0aaf16a61ff627dc766fa62c31fc63
Pratiksha Tupe <pratikshatupe2021@gmail.com>
2026-02-23 16:52:14 +0530

    change path in layout.jsx page

--- src/component/admin/Layout.jsx
+++ src/component/admin/Layout.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "../../component/admin/SideBar";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
